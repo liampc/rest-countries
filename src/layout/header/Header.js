cf9c3e43a4e22edacff90fefceaef8481ff9b21d
liampc <kathliampcarlos@gmail.com>
2021-03-25 14:33:09 +0800

    add header banner

--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
+import './_header.scss'
 
 const Header = () => {
     return (
-        <header>
-            this is header 
+        <header className="header">
+            <div className="header__banner container">
+                <h1 className="header__logo">Where in the world?</h1>
+                <p className="header__switch">
+                    <i><ion-icon name="moon-outline"></ion-icon></i>
+                    <span>Dark Mode</span>
+                </p>
+            </div>
         </header>
     )
 }
