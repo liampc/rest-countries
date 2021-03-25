0894c8f7eecd129feffde04902370198dfe2643e
liampc <kathliampcarlos@gmail.com>
2021-03-25 19:48:16 +0800

    add initial select

--- src/layout/main/Main.js
+++ src/layout/main/Main.js
@@ -5,10 +5,22 @@ const Main = () => {
     return (
         <main className="main">
             <nav className="nav container">
-                <form className="searchBar">
-                    <i><ion-icon name="search-outline"></ion-icon></i>
-                    <input type="text" placeholder="Search for a country..." />
-                </form>
+                <div className="nav__search">
+                    <form className="searchBar">
+                        <i><ion-icon name="search-outline"></ion-icon></i>
+                        <input type="text" placeholder="Search for a country..." />
+                    </form>
+                </div>
+                <div className="nav__select">
+                    <select className="select" name="region" id="region">
+                        <option className="select" value="africa">Africa</option>
+                        <option value="america">America</option>
+                        <option value="asia">Asia</option>
+                        <option value="europe">Europe</option>
+                        <option value="oceania">Oceania</option>
+                    </select>
+                </div>
+                
             </nav>
             
         </main>
