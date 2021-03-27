a5574b4f9780b25774248cadf62ee18ec275da3d
liampc <kathliampcarlos@gmail.com>
2021-03-27 10:01:13 +0800

    make header dark mode

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
 import './App.scss';
+import React, {useState, useEffect} from 'react'
 import Header from './layout/header/Header'
 import Main from './layout/main/Main'
 
 function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  const switchToDark = () => {
+
+    setDarkMode(!darkMode)
+  
+    let elems = document.querySelectorAll('.to-switch')
+    let mode = document.querySelector('#mode')
+    elems.forEach(el => {
+      if (darkMode == true){
+        el.classList.add('dark')
+        mode.innerHTML = 'Light'
+      }
+      else {
+        el.classList.remove('dark')
+        mode.innerHTML = 'Dark'
+      }
+    })
+    
+
+  }
+
+
+
   return (
     <div className="App">
-      <Header />
+      <Header handleClick={switchToDark} />
       <Main />
     </div>
   );
--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import './_header.scss'
 
-const Header = () => {
+
+
+const Header = (props) => {
     return (
-        <header className="header">
+        <header className="header to-switch">
             <div className="header__banner container">
                 <h1 className="header__logo">Where in the world?</h1>
-                <p className="header__switch">
-                    <i><ion-icon name="moon-outline"></ion-icon></i>
-                    <span>Dark Mode</span>
+                <p className="header__switch to-switch" onClick={props.handleClick}>
+                    {/* <i><ion-icon name="moon-outline"></ion-icon></i> */}
+                    <i></i>
+                    <p><span id="mode">Dark</span> Mode</p>
                 </p>
             </div>
         </header>
