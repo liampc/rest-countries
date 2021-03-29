852d28095800733f395016eec6734846c1db3709
liampc <kathliampcarlos@gmail.com>
2021-03-29 11:01:50 +0800

    edit route for app

--- src/App.js
+++ src/App.js
@@ -6,33 +6,10 @@ import Page from './layout/page/Page'
 
 function App() {
 
-  const [darkMode, setDarkMode] = useState(false)
-
-  const switchToDark = () => {
-
-    setDarkMode(!darkMode)
-  
-    let elems = document.querySelectorAll('.to-switch')
-    let mode = document.querySelector('#mode')
-    elems.forEach(el => {
-      if (darkMode == true){
-        el.classList.add('dark')
-        mode.innerHTML = 'Light'
-      }
-      else {
-        el.classList.remove('dark')
-        mode.innerHTML = 'Dark'
-      }
-    })
-    
-
-  }
-
-
 
   return (
     <div className="App">
-      <Header handleClick={switchToDark} />
+      <Header />
       <Main />
       <Page />
     </div>
--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './_header.scss'
 
 
 
-const Header = (props) => {
+const Header = () => {
+
+    const [darkMode, setDarkMode] = useState(false)
+
+    const switchToDark = () => {
+
+    setDarkMode(!darkMode)
+  
+    let elems = document.querySelectorAll('.to-switch')
+    let mode = document.querySelector('#mode')
+    elems.forEach(el => {
+      if (darkMode == true){
+        el.classList.add('dark')
+        mode.innerHTML = 'Light'
+      }
+      else {
+        el.classList.remove('dark')
+        mode.innerHTML = 'Dark'
+      }
+    })
+    
+
+  }
+
     return (
         <header className="header to-switch">
             <div className="header__banner container">
                 <h1 className="header__logo">Where in the world?</h1>
-                <p className="header__switch to-switch" onClick={props.handleClick}>
+                <p className="header__switch to-switch" onClick={switchToDark}>
                     {/* <i><ion-icon name="moon-outline"></ion-icon></i> */}
                     <i></i>
                     <p><span id="mode">Dark</span> Mode</p>
