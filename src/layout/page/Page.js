02582a445a1addc07258313c36bdb688b5947c05
liampc <kathliampcarlos@gmail.com>
2021-03-30 14:31:13 +0800

    add array data

--- src/layout/page/Page.js
+++ src/layout/page/Page.js
@@ -4,20 +4,27 @@ import image from '../../assets/germany.png'
 
 const Page = ( {match} ) => {
 
-    const [country, setCountry] = useState()
+    const [country, setCountry] = useState([])
+    const [currency, setCurrency] = useState('')
+    const [languages, setLanguages] = useState([])
 
     useEffect(() => {
-        // fetchCountry()
+        fetchCountry()
         
     }, [])
 
 
-    // const fetchCountry = async () => {
-    //     const response = await fetch(`https://restcountries.eu/rest/v2/name/${match.params.id}?fullText=true`)
-    //     const data = await response.json()
-    //     console.log(data[0])
-    //     setCountry(data[0])
-    // }
+    const fetchCountry = async () => {
+        const response = await fetch(`https://restcountries.eu/rest/v2/name/${match.params.id}?fullText=true`)
+        const data = await response.json()
+        console.log(data[0])
+        setCountry(data[0])
+        setCurrency(data[0].currencies[0].name)
+        setLanguages(data[0].languages)
+    }
+
+   
+
 
     return (
         <div className="page">
@@ -30,19 +37,29 @@ const Page = ( {match} ) => {
                 <div className="detail">
                     <div className="detail__flag" style={{backgroundImage: `url(${image})`}}></div>
                     <div className="detail__info">
-                        <h2>Belgium</h2>
+                        <h2>{country.name}</h2>
                         <div className="detail__group">
                             <div>
-                                <p>Native Name: <span>Belgie</span></p>
-                                <p>Population: <span>11,319,511</span></p>
-                                <p>Region: <span>Europe</span></p>
-                                <p>Subregion: <span>Western Europe</span></p>
-                                <p>Capital: <span>Brussels</span></p>
+                                <p>Native Name: <span>{country.nativeName}</span></p>
+                                <p>Population: <span>{country.population}</span></p>
+                                <p>Region: <span>{country.region}</span></p>
+                                <p>Subregion: <span>{country.subregion}</span></p>
+                                <p>Capital: <span>{country.capital}</span></p>
                             </div>
                             <div>
-                                <p>Top Level Domain: <span>Belgie</span></p>
-                                <p>Currencies: <span>11,319,511</span></p>
-                                <p>Languages: <span>Dutch, French, German </span></p>
+                                <p>Top Level Domain: <span>{country.topLevelDomain}</span></p>
+                                <p>Currencies: <span>{currency}</span></p>
+                                <p>Languages: 
+                                    {languages.map((lang, index) => {
+    
+                                        if (index === languages.length - 1){
+                                            return <span> {lang.name}</span>
+                                        } else {
+                                            return <span> {lang.name}, </span>
+                                        }
+                                      
+                                    })}
+                                </p>
                             </div>
                         </div>
                         <div className="detail__border">
