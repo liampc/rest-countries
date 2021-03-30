df51825afe4bdfbcd5998c0d877f3f7b1589040c
liampc <kathliampcarlos@gmail.com>
2021-03-30 21:10:08 +0800

    move switchtodark func in app component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import './App.scss';
 import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './layout/header/Header'
 import Main from './layout/main/Main'
 import Page from './layout/page/Page'
 
 function App() {
 
+  const [darkMode, setDarkMode] = useState(false)
+
+    const switchToDark = () => {
+
+    setDarkMode(!darkMode)
+  
+    let elems = document.querySelectorAll('.to-switch')
+    let mode = document.querySelector('#mode')
+    elems.forEach(el => {
+      if (darkMode == true){
+        el.classList.add('dark')
+        mode.innerHTML = 'Light'
+      }
+      else {
+        el.classList.remove('dark')
+        mode.innerHTML = 'Dark'
+      }
+    })
+
+  }
+
 
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Page />
-    </div>
+    <Router>
+      <div className="App">
+        <Header switchToDark={switchToDark} />
+        <Route>
+          <Route exact path="/" component={Main} />
+          <Route path="/:id" component={Page} />
+        </Route>
+      </div>
+    </Router>
   );
 }
 
--- src/layout/header/Header.js
+++ src/layout/header/Header.js
@@ -4,36 +4,13 @@ import {Link} from 'react-router-dom'
 
 
 
-const Header = () => {
-
-    const [darkMode, setDarkMode] = useState(false)
-
-    const switchToDark = () => {
-
-    setDarkMode(!darkMode)
-  
-    let elems = document.querySelectorAll('.to-switch')
-    let mode = document.querySelector('#mode')
-    elems.forEach(el => {
-      if (darkMode == true){
-        el.classList.add('dark')
-        mode.innerHTML = 'Light'
-      }
-      else {
-        el.classList.remove('dark')
-        mode.innerHTML = 'Dark'
-      }
-    })
-    
-
-  }
+const Header = (props) => {
 
     return (
         <header className="header to-switch">
             <div className="header__banner container">
                 <Link to={'/'}><h1 className="header__logo">Where in the world?</h1></Link>
-                <p className="header__switch to-switch" onClick={switchToDark}>
-                    {/* <i><ion-icon name="moon-outline"></ion-icon></i> */}
+                <p className="header__switch to-switch" onClick={props.switchToDark}>
                     <i></i>
                     <p><span id="mode">Dark</span> Mode</p>
                 </p>
